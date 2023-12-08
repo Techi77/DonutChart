9f8e5a380df1d560593e88acdef7023ece094828
Kate Rossinskaya <kateros@siber.com>
2023-12-08 15:36:04 +0100

    add: striped arc

--- app/src/main/java/com/example/donutchart/MainActivity.kt
+++ app/src/main/java/com/example/donutchart/MainActivity.kt
@@ -4,20 +4,27 @@ package com.example.donutchart
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.donutchart.databinding.MainActivityLayoutBinding
@@ -56,7 +63,8 @@ class MainActivity : AppCompatActivity() {
 fun PieChart(
     data: Map<String, Int>,
     radiusOuter: Dp = 40.dp,
-    chartBarWidth: Dp = 16.dp
+    chartBarWidth: Dp = 16.dp,
+    backgroundColor: Color= White
 ) {
 
     val totalSum = data.values.sum()
@@ -77,6 +85,14 @@ fun PieChart(
             .size((radiusOuter.value * 2f).dp),
         contentAlignment = Alignment.Center
     ) {
+        Image(
+            painter = painterResource(R.drawable.chart_background),
+            contentDescription = "avatar",
+            contentScale = ContentScale.Crop,            // crop the image if it's not a square
+            modifier = Modifier
+                .size(radiusOuter * 2f)
+                .clip(CircleShape)                       // clip to the circle shape
+        )
         Canvas(
             modifier = Modifier
                 .size(radiusOuter * 2f)
@@ -93,6 +109,10 @@ fun PieChart(
                 )
                 lastValue += value
             }
+            drawCircle(
+                color=backgroundColor,
+                radius = (radiusOuter-chartBarWidth).toPx()
+            )
         }
     }
 }
